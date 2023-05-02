40742b4dfe0606e31b46df5e229293829e35f84e
Kedar Vyas <kedarvyas02@gmail.com>
2023-05-02 17:47:06 +0530

    initial commit

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam,
+        deserunt? Voluptate doloribus ea aspernatur modi ullam explicabo commodi
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo,
+        quia corporis ex molestias nemo quos ad, aperiam consectetur incidunt
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
